25c18b49c5f2358b3a64ebbba8c83a23756df024
bitbytebit <tribe321@gmail.com>
2020-03-28 23:36:29 +0100

    Dynamic slots

--- src/views/AdminEditor.vue
+++ src/views/AdminEditor.vue
@@ -5,6 +5,127 @@
       :items="cItems"
       :headers="cHeaders"
     >
+      <template v-slot:top>
+        <v-toolbar
+          flat
+        >
+          <v-toolbar-title>Total - {{ items.length }}</v-toolbar-title>
+          <v-divider
+            class="mx-4"
+            inset
+            vertical
+          />
+          <v-toolbar-title>Last - {{ items[items.length-1].id }}</v-toolbar-title>
+          <v-divider
+            class="mx-4"
+            inset
+            vertical
+          />
+          <v-toolbar-title>Types - {{ items[items.length-1].id }}</v-toolbar-title>
+          <v-divider
+            class="mx-4"
+            inset
+            vertical
+          />
+          <v-spacer />
+          <v-btn
+            color="primary"
+            dark
+            class="mb-2 mr-2"
+            @click="saveFile(localItems)"
+          >
+            Save
+          </v-btn>
+          <v-dialog
+            v-model="dialog"
+            max-width="600px"
+          >
+            <template v-slot:activator="{ on }">
+              <v-btn
+                color="primary"
+                dark
+                class="mb-2"
+                v-on="on"
+              >
+                New Item
+              </v-btn>
+            </template>
+            <v-card>
+              <v-card-title>
+                <span class="headline">{{ formTitle }}</span>
+              </v-card-title>
+
+              <v-card-text>
+                <v-container>
+                  <v-row>
+                    <v-col
+                      cols="12"
+                      sm="6"
+                      md="12"
+                      :key="i"
+                      v-for="(item, i) in editedItem"
+                    >
+                      <v-textarea
+                        v-if="i == 'description'"
+                        v-model="editedItem[i]"
+                        :label="i"
+                        hide-details
+                      />
+                      <v-text-field
+                        v-else
+                        v-model="editedItem[i]"
+                        :label="i"
+                        hide-details
+                      />
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-spacer />
+                <v-btn
+                  color="blue darken-1"
+                  text
+                  @click="close"
+                >
+                  Cancel
+                </v-btn>
+                <v-btn
+                  color="blue darken-1"
+                  text
+                  @click="save"
+                >
+                  Save
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-toolbar>
+      </template>
+      <template v-slot:item.actions="{ item }">
+        <v-icon
+          small
+          class="mr-2"
+          @click="editItem(item)"
+        >
+          mdi-pencil
+        </v-icon>
+        <v-icon
+          small
+          @click="deleteItem(item)"
+        >
+          mdi-delete
+        </v-icon>
+      </template>
+      <template v-slot:no-data>
+        <v-btn
+          color="primary"
+          @click="initialize"
+        >
+          Reset
+        </v-btn>
+      </template>
       <template
         v-for="slot in slots"
         v-slot:[slot]="scope"
