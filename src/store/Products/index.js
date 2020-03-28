6375bbc73184188659af5b2d61a6287b10aed796
bitbytebit <tribe321@gmail.com>
2020-03-28 05:36:00 +0100

    Update index.js

--- src/store/Products/index.js
+++ src/store/Products/index.js
@@ -2,17 +2,17 @@ export default {
   namespaced: true,
   state: {
     products: [
-      { url: null, id: 'Facemask-01', name: 'Facemask', description: 'Make a statement with this stylish facemask.\nMaterial: Cotton\nSize: 14.5*11 cm / 5.7*4.3 inch', type: 'clothing', price: '10', image: 'facemask.png' },
-      { url: null, id: 'Facemask-02', name: 'Facemask', description: 'Make a statement with this stylish facemask.\nMaterial: Cotton\nSize: 14.5*11 cm / 5.7*4.3 inch', type: 'clothing', price: '10', image: 'facemask.png' },
-      { url: null, id: 'Facemask-02', name: 'Facemask', description: 'Make a statement with this stylish facemask.\nMaterial: Cotton\nSize: 14.5*11 cm / 5.7*4.3 inch', type: 'clothing', price: '10', image: 'facemask.png' },
-      { url: null, id: 'Facemask-02', name: 'Facemask', description: 'Make a statement with this stylish facemask.\nMaterial: Cotton\nSize: 14.5*11 cm / 5.7*4.3 inch', type: 'clothing', price: '10', image: 'facemask.png' },
-      { url: null, id: 'Facemask-02', name: 'Facemask', description: 'Make a statement with this stylish facemask.\nMaterial: Cotton\nSize: 14.5*11 cm / 5.7*4.3 inch', type: 'clothing', price: '10', image: 'facemask.png' },
-      { url: null, id: 'Facemask-02', name: 'Facemask', description: 'Make a statement with this stylish facemask.\nMaterial: Cotton\nSize: 14.5*11 cm / 5.7*4.3 inch', type: 'clothing', price: '10', image: 'facemask.png' },
-      { url: null, id: 'Facemask-02', name: 'Facemask', description: 'Make a statement with this stylish facemask.\nMaterial: Cotton\nSize: 14.5*11 cm / 5.7*4.3 inch', type: 'clothing', price: '10', image: 'facemask.png' },
-      { url: null, id: 'Facemask-02', name: 'Facemask', description: 'Make a statement with this stylish facemask.\nMaterial: Cotton\nSize: 14.5*11 cm / 5.7*4.3 inch', type: 'clothing', price: '10', image: 'facemask.png' },
-      { url: null, id: 'Facemask-03', name: 'Facemask', description: 'Make a statement with this stylish facemask.\nMaterial: Cotton\nSize: 14.5*11 cm / 5.7*4.3 inch', type: 'clothing', price: '10', image: 'facemask.png' },
-      { url: null, id: 'Facemask-04', name: 'Facemask', description: 'Make a statement with this stylish facemask.\nMaterial: Cotton\nSize: 14.5*11 cm / 5.7*4.3 inch', type: 'clothing', price: '10', image: 'facemask.png' },
-      { url: null, id: 'Facemask-04', name: 'Facemask', description: 'Make a statement with this stylish facemask.\nMaterial: Cotton\nSize: 14.5*11 cm / 5.7*4.3 inch', type: 'albums', price: '10', image: 'facemask.png' }
+      { url: null, id: 'Facemask-0', name: 'Facemask', description: 'Make a statement with this stylish facemask.\nMaterial: Cotton\nSize: 14.5*11 cm / 5.7*4.3 inch', type: 'clothing', price: '10', image: 'facemask.png' },
+      { url: null, id: 'Facemask-1', name: 'Facemask', description: 'Make a statement with this stylish facemask.\nMaterial: Cotton\nSize: 14.5*11 cm / 5.7*4.3 inch', type: 'clothing', price: '10', image: 'facemask.png' },
+      { url: null, id: 'Facemask-2', name: 'Facemask', description: 'Make a statement with this stylish facemask.\nMaterial: Cotton\nSize: 14.5*11 cm / 5.7*4.3 inch', type: 'clothing', price: '10', image: 'facemask.png' },
+      { url: null, id: 'Facemask-3', name: 'Facemask', description: 'Make a statement with this stylish facemask.\nMaterial: Cotton\nSize: 14.5*11 cm / 5.7*4.3 inch', type: 'clothing', price: '10', image: 'facemask.png' },
+      { url: null, id: 'Facemask-4', name: 'Facemask', description: 'Make a statement with this stylish facemask.\nMaterial: Cotton\nSize: 14.5*11 cm / 5.7*4.3 inch', type: 'clothing', price: '10', image: 'facemask.png' },
+      { url: null, id: 'Facemask-5', name: 'Facemask', description: 'Make a statement with this stylish facemask.\nMaterial: Cotton\nSize: 14.5*11 cm / 5.7*4.3 inch', type: 'clothing', price: '10', image: 'facemask.png' },
+      { url: null, id: 'Facemask-6', name: 'Facemask', description: 'Make a statement with this stylish facemask.\nMaterial: Cotton\nSize: 14.5*11 cm / 5.7*4.3 inch', type: 'clothing', price: '10', image: 'facemask.png' },
+      { url: null, id: 'Facemask-7', name: 'Facemask', description: 'Make a statement with this stylish facemask.\nMaterial: Cotton\nSize: 14.5*11 cm / 5.7*4.3 inch', type: 'clothing', price: '10', image: 'facemask.png' },
+      { url: null, id: 'Facemask-8', name: 'Facemask', description: 'Make a statement with this stylish facemask.\nMaterial: Cotton\nSize: 14.5*11 cm / 5.7*4.3 inch', type: 'clothing', price: '10', image: 'facemask.png' },
+      { url: null, id: 'Facemask-9', name: 'Facemask', description: 'Make a statement with this stylish facemask.\nMaterial: Cotton\nSize: 14.5*11 cm / 5.7*4.3 inch', type: 'clothing', price: '10', image: 'facemask.png' },
+      { url: null, id: 'Facemask-10', name: 'Facemask', description: 'Make a statement with this stylish facemask.\nMaterial: Cotton\nSize: 14.5*11 cm / 5.7*4.3 inch', type: 'albums', price: '10', image: 'facemask.png' }
     ]
   },
   getters: {
